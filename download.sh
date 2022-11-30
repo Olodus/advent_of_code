4550c650158cc33342126420338b266389bb03a0
Olof Düsterdieck <olof.dusterdieck@gmail.com>
2022-11-30 22:11:54 +0100

    Move all Days into 2021 dir
    Also updated download script to recognize year directory.
    This is to prepare for moving all my previous year's solutions into this
    repo.
    In a few hours I will start working on 2022's problems in this repo as
    well.

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p Day"$AOC_DAY"
-curl https://adventofcode.com/"$AOC_YEAR"/day/"$AOC_DAY"/input --cookie "session=$AOC_SESSION" > Day"$AOC_DAY"/input.txt
+mkdir -p "$AOC_YEAR"/Day"$AOC_DAY"
+curl https://adventofcode.com/"$AOC_YEAR"/day/"$AOC_DAY"/input --cookie "session=$AOC_SESSION" > "$AOC_YEAR"/Day"$AOC_DAY"/input.txt
